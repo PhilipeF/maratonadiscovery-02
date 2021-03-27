0408ee3236e27388042f6153541276810c902871
Philipe de Souza Ferreira <philipesferreiraa@gmail.com>
2021-03-27 00:11:50 -0300

    working template engine

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const server = express()
+const routes = require('./routes')
+
+//utilizando templante engine
+server.set('view engine', 'ejs')
+
+//habilitar arquivos statics
+server.use(express.static('public'))
+
+//routes
+server.use(routes)
+
+server.listen(3000, () => console.log('rodando'))
+
+
+
+
