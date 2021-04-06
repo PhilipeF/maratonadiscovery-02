5c22592208f4f784f70a8a5bf72e3f0c9f82dafb
Philipe de Souza Ferreira <philipesferreiraa@gmail.com>
2021-04-06 20:19:27 -0300

    review

--- src/routes.js
+++ src/routes.js
@@ -4,17 +4,25 @@ const routes = express.Router();
 const views = __dirname + '/views/';
 
 const profile = {
-    name: "Goku",
-    avatar: "https://w7.pngwing.com/pngs/835/388/png-transparent-goku-goten-vegeta-chibi-drawing-goku-child-manga-computer-wallpaper.png",
+    name: "Philipe",
+    avatar: "http://github.com/philipef.png",
     "monthly-budget": 3000,
     "days-per-week": 5,
     "hours-per-day": 5,
     "vacation-per-year": 4
 }
 
+const jobs = []  
+
 routes.get('/', (req, res) => res.render(views + 'index'));
 routes.get('/job/edit', (req, res) => res.render(views + 'job-edit'));
 routes.get('/job', (req, res) => res.render(views + 'job'));
+routes.post('/job', (req, res) => {
+    jobs.push(req.body)
+    return res.redirect('/')
+    
+});
+
 routes.get('/profile', (req, res) => res.render(views + 'profile', { profile }));
 
 routes.get('/index', (req, res) => {
